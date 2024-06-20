e80bde57887683876d229809bd2d7e7b96d8fd6a
Luca Lacerda <lucagabriel520@gmail.com>
2024-06-20 20:45:30 -0300

    views fold reference

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const mustacheExpress = require('mustache-express');
 const app = express();
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/src/views');
+app.use(express.urlencoded({extended: true}));
 
 const PORT = 8080;
 
