c006ad26452edfb5a6d59612cde2b6c98fc2cf7c
Luca Lacerda <lucagabriel520@gmail.com>
2024-06-20 21:14:54 -0300

    sequelize and db install

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
+const db = require('./src/db')
 const app = express();
+const PORT = 8080;
+
 
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/src/views');
 app.use(express.urlencoded({extended: true}));
 
-const PORT = 8080;
+db.sync(() => console.log('Conectado ao banco de dados'));
 
 app.listen(PORT,function() {
     console.log('Hospedado na porta ' + PORT);
---
+++ src/db.js
@@ -0,0 +1,8 @@
+ const Sequelize = require('sequelize');
+
+ const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    Storage: './database.sqlite'
+ });
+
+ module.exports = sequelize;
