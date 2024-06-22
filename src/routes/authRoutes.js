dd59c27d57ea72fa51caee2b7a8f614792bb8885
SamuelMTL <95709293+SamuelMTL@users.noreply.github.com>
2024-06-22 15:42:25 -0300

    Revert "."
    This reverts commit c3716189ee6dfcaceebeb092b3bbe7a25d8805c2.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const authMiddleware = require('../middleware/authMiddleware');
 
 router.get('/register', authController.getRegister);
 router.post('/register', authController.postRegister);
@@ -9,6 +8,6 @@ router.post('/register', authController.postRegister);
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
 
-router.post('/updateProfile', authMiddleware, authController.updateProfile);
+router.post('/updateProfile', authController.updateProfile);
 
 module.exports = router;
