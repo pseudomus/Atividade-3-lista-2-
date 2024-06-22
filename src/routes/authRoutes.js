c3716189ee6dfcaceebeb092b3bbe7a25d8805c2
SamuelMTL <95709293+SamuelMTL@users.noreply.github.com>
2024-06-22 15:05:31 -0300

    .

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 router.get('/register', authController.getRegister);
 router.post('/register', authController.postRegister);
@@ -8,6 +9,6 @@ router.post('/register', authController.postRegister);
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
 
-router.post('/updateProfile', authController.updateProfile);
+router.post('/updateProfile', authMiddleware, authController.updateProfile);
 
 module.exports = router;
