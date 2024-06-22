0d58a66ceea449dc5da7f47deb92e3082e9fec3b
Luca <paesberaldiluca@gmail.com>
2024-06-22 13:56:16 -0300

    Arrumei o fato de ter apagado a route da tela de início. :p

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { requireAuth } = require('../middleware/authMiddleware');
 const mainController = require('../controllers/mainController');
+
+router.get('/telaDeInicio', requireAuth, (req, res) => {
+    res.render('telaDeInicio', { user: req.user });
+});
 
 // Rota para a página inicial
 router.get('/', mainController.getHome);
